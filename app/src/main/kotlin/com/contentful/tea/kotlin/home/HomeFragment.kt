70779a490805d9848480df40ce4b2c60efb11cb7
Mario Bodemann <mario@contentful.com>
2018-08-02 11:19:07 +0200

    restore settings on start of app

--- app/src/main/kotlin/com/contentful/tea/kotlin/home/HomeFragment.kt
+++ app/src/main/kotlin/com/contentful/tea/kotlin/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.contentful.tea.kotlin.home
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.MenuItem
@@ -11,12 +13,18 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import com.contentful.tea.kotlin.R
+import com.contentful.tea.kotlin.contentful.Api
+import com.contentful.tea.kotlin.contentful.Contentful
+import com.contentful.tea.kotlin.contentful.EditorialFeature
 import com.contentful.tea.kotlin.contentful.Layout
 import com.contentful.tea.kotlin.contentful.LayoutModule
+import com.contentful.tea.kotlin.contentful.Parameter
 import com.contentful.tea.kotlin.dependencies.Dependencies
 import com.contentful.tea.kotlin.dependencies.DependenciesProvider
 import com.contentful.tea.kotlin.extensions.setImageResourceFromUrl
 import com.contentful.tea.kotlin.extensions.showError
+import com.contentful.tea.kotlin.extensions.toHtml
+import com.contentful.tea.kotlin.extensions.toast
 import kotlinx.android.synthetic.main.course_card.view.*
 import kotlinx.android.synthetic.main.fragment_home.*
 
@@ -44,19 +52,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        dependencies
-            .contentful
-            .fetchHomeLayout(errorCallback = ::errorFetchingLayout) { layout: Layout ->
-                layout.contentModules.forEach { module ->
-                    activity?.runOnUiThread {
-                        layoutInflater.inflate(R.layout.course_card, home_courses, false).apply {
-                            updateModuleView(this, module)
-                            home_courses?.addView(this)
-                        }
-                    }
-                }
-            }
-
         main_bottom_navigation.setOnNavigationItemSelectedListener {
             if (activity != null) {
                 bottomNavigationItemSelected(it)
@@ -65,6 +60,30 @@ class HomeFragment : Fragment() {
                 false
             }
         }
+
+        activity?.apply {
+            val preferences =
+                getSharedPreferences("${packageName}_preferences", Context.MODE_PRIVATE)
+            dependencies.contentful.applyParameterFromSharedPreferences(preferences) {
+                loadHomeView()
+            }
+        }
+    }
+
+    private fun loadHomeView() {
+        dependencies
+            .contentful
+            .fetchHomeLayout(errorCallback = ::errorFetchingLayout) { layout: Layout ->
+                layout.contentModules.forEach { module ->
+                    activity?.runOnUiThread {
+                        layoutInflater.inflate(R.layout.course_card, home_courses, false)
+                            .apply {
+                                updateModuleView(this, module)
+                                home_courses?.addView(this)
+                            }
+                    }
+                }
+            }
     }
 
     private fun updateModuleView(view: View, module: LayoutModule) {
@@ -137,5 +156,41 @@ class HomeFragment : Fragment() {
                 }
             )
         }
+    }
+
+    private fun Contentful.applyParameterFromSharedPreferences(
+        preferences: SharedPreferences,
+        successCallback: () -> Unit
+    ) {
+        val parameter = Parameter(
+            editorialFeature =
+            if (preferences.getBoolean(getString(R.string.settings_key_editorial), false)) {
+                EditorialFeature.Enabled
+            } else {
+                EditorialFeature.Disabled
+            },
+            api = Api.valueOf(
+                preferences.getString(
+                    getString(R.string.settings_key_api),
+                    Api.CDA.name
+                )
+            ),
+            locale = preferences.getString(getString(R.string.settings_key_locale), "en-US")
+        )
+
+        applyParameter(
+            parameter,
+            errorHandler = { activity?.toast(getString(R.string.error_settings_cannot_change)) },
+            successHandler = { space ->
+                activity?.toast(
+                    getString(
+                        R.string.settings_connected_successfully_to_space,
+                        space.name()
+                    ).toHtml()
+                )
+
+                successCallback()
+            }
+        )
     }
 }
